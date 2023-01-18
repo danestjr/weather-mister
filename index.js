3dbc08e3159e8eaa19c839ce1d3f77fc2e6003bf
Daniel Estrada <daniel@estradacodes.com>
2023-01-17 20:46:32 -0800

    add express static dir

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ dotenv.config()
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.json())
 
+app.use(express.static('client/build'));
+
+
 app.get('*', (req, res) => {
   res.sendFile(
       path.resolve(__dirname, 'client', 'build', 
